11c4e88a793ba8f4e8c26753d96fe798772e1fe6
Cathy Cullen <c_cullen.cathy@cerner.com>
2019-03-04 17:26:28 -0600

    add seeds and index action

--- app/controllers/baits_controller.rb
+++ app/controllers/baits_controller.rb
@@ -7,45 +7,4 @@ class BaitsController < ApplicationController
 
     render json: @baits
   end
-
-  # GET /baits/1
-  def show
-    render json: @bait
-  end
-
-  # POST /baits
-  def create
-    @bait = Bait.new(bait_params)
-
-    if @bait.save
-      render json: @bait, status: :created, location: @bait
-    else
-      render json: @bait.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /baits/1
-  def update
-    if @bait.update(bait_params)
-      render json: @bait
-    else
-      render json: @bait.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /baits/1
-  def destroy
-    @bait.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bait
-      @bait = Bait.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def bait_params
-      params.require(:bait).permit(:name, :category, :image)
-    end
 end
